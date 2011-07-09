49e114048ed4f06ccf23367cec7fc6449ccc4b84
Yoni Shalom <silver83@gmail.com>
2011-07-10 00:30:43 +0300

    some tests and beginning of actual scans

---
+++ Z.Projects/ProjectScanner.cs
@@ -0,0 +1,48 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Z.Projects.Scanner;
+using System.IO;
+
+namespace Z.Projects
+{
+    public interface IScanClueProvider
+    {
+        IEnumerable<IScanClue<T>> GetScanClues<T>(Type t);
+    }
+
+    /// <summary>
+    /// Takes a directory and finds projects
+    /// </summary>
+    public class ProjectScanner
+    {
+        public IEnumerable<IProject> Scan(string directory, IScanClueProvider scanClueProvider)
+        {
+            IEnumerable<IScanClue<string>> clues = scanClueProvider.GetScanClues<string>(typeof(FileNameScanner));
+            IEnumerable<string> files = ScanDirectory(directory);
+
+            FileNameScanner scanner = new FileNameScanner(clues);
+            var matchedClues = scanner.Scan(files); 
+            foreach (var matchedClue in matchedClues)
+            {
+                Console.WriteLine(matchedClue);
+            }
+
+            yield break;
+        }
+
+        private IEnumerable<string> ScanDirectory(string directory)
+        {
+            string[] files = Directory.GetFiles(directory);
+            foreach (var file in files)
+                yield return file;
+
+            string[] subdirs = Directory.GetDirectories(directory);
+            foreach (var dir in subdirs)
+                foreach (var file in ScanDirectory(dir))
+                    yield return file;
+        }
+    }
+}
---
+++ Z.Tests/ProjectTests.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using System.IO;
+using Rhino.Mocks;
+using Z.Projects.Scanner;
+using Z.Projects;
+
+namespace Z.Tests
+{
+    [TestFixture]
+    public class ProjectTests
+    {
+        [Test]
+        public void start()
+        {
+            string dir = Directory.GetCurrentDirectory();
+            dir = Directory.GetParent(dir).Parent.FullName;
+
+            List<IScanClue<string>> clues = new List<IScanClue<string>>() { new FileNameScanClue("*.csproj") };
+
+            IScanClueProvider provider = MockRepository.GenerateMock<IScanClueProvider>();
+            provider.Expect(x=>provider.GetScanClues<string>(typeof(string)))
+                .IgnoreArguments()
+                .Return(clues);
+
+            ProjectScanner scanner = new ProjectScanner();
+            IEnumerable<IProject> projects = scanner.Scan(dir, provider);
+            projects.ToList();
+
+            //pipeline :
+            //directory -> enumerate all files and subdirs -> matches filenames -> configuration keys -> Projects 
+            //IEnumerable<string> directories;
+
+            //IEnumerable<KeyValuePair<IProject, IProjectFile>> matchedFiles = 
+            //                                                        from d in directories
+            //                                                        group d by 
+            //                                                        group 
+
+            //IEnumerable<KeyValuePair<IProject, IProjectComponent> a = from f in matchedFiles
+            //                                                      select f.Parse();
+
+
+            //IEnumerable<IProject> project = 
+        }
+    }
+}
