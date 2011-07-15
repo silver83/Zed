dafed2792d92bea4b29ab16a12878f12ae21984b
Yoni Shalom <silver83@gmail.com>
2011-07-16 02:17:01 +0300

    back to work.. some stuff

---
+++ Z.Algorithms/ReferenceManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+using Z.Common.Logging;
+
+namespace Z.Algorithms
+{
+    public class ReferenceManager
+    {
+        private static void VerifyReferences(Assembly asm, Dictionary<string, bool> history)
+        {
+            if (history.ContainsKey(asm.FullName))
+                return;
+
+            Log.Application.InfoFormat("verifying assembly references for {0}", asm.FullName);
+            history.Add(asm.FullName, true);
+
+            foreach (var refAsmName in asm.GetReferencedAssemblies())
+            {
+                Assembly refAsm = Assembly.Load(refAsmName);
+                VerifyReferences(refAsm, history);
+            }
+        }
+
+        public static void VerifyReferences(Assembly asm)
+        {
+            VerifyReferences(asm, new Dictionary<string,bool>());
+        }
+    }
+}
---
+++ Z.Tests/ReferenceVerificationTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using Z.Algorithms;
+using Z.Common;
+using System.Reflection;
+using System.IO;
+
+namespace Z.Tests
+{
+    [TestFixture]
+    public class ReferenceVerificationTest
+    {
+        [Test]
+        [Ignore]
+        public void Test()
+        {
+            Infrastructure.Load();
+            ReferenceManager.VerifyReferences(Assembly.LoadFile(Path.Combine(Directory.GetCurrentDirectory(), "test\\Z.Tests.dll")));
+        }
+    }
+}
